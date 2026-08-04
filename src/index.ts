11c577583174fbb0efda2385473c7ce480617a6a
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-04 10:07:50 +0100

    Export password audit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // @ts-check
 
 export { Id } from "./id.ts";
-export { open, pack, pwdOk, readPub } from "./core.ts";
+export { auditPwd, open, pack, pwdOk, readPub } from "./core.ts";
+export { pwdMin } from "./pwd.ts";
